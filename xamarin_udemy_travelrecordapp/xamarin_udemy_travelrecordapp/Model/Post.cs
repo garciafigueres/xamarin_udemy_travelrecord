fb9c96f8fe1a92a44e2e9175502891af6afb7712
Luis García Figueres <garciafigueres@gmail.com>
2020-04-14 09:28:10 +0200

    added database model

---
+++ xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/Model/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarin_udemy_travelrecordapp.Model
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Experience { get; set; }
+    }
+}
