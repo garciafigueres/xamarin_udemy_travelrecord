b75933719f7742165a4359937ac6bc95962ff0f0
Luis García Figueres <garciafigueres@gmail.com>
2020-04-14 09:29:38 +0200

    sqlite attributes

--- xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/Model/Post.cs
+++ xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/Model/Post.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SQLite;
 
 namespace xamarin_udemy_travelrecordapp.Model
 {
     public class Post
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
+        [MaxLength(250)]
         public string Experience { get; set; }
     }
 }
