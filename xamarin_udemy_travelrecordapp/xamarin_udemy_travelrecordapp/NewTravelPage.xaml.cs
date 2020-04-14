236ecdab66a2d1baf122f413539f929ca5c675a8
Luis García Figueres <garciafigueres@gmail.com>
2020-04-14 09:36:50 +0200

    inserting into database

--- xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/NewTravelPage.xaml.cs
+++ xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/NewTravelPage.xaml.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
+using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using xamarin_udemy_travelrecordapp.Model;
 
 namespace xamarin_udemy_travelrecordapp
 {
@@ -15,6 +13,24 @@ namespace xamarin_udemy_travelrecordapp
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = experienceEntry.Text
+            };
+
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Post>();
+            int rows = conn.Insert(post);
+            conn.Close();
+
+            if (rows > 0)
+                DisplayAlert("Success", "Experience succesfully inserted", "Ok");
+            else
+                DisplayAlert("Failure", "Experience failed to be inserted", "Ok");
         }
     }
 }
