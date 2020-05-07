3800dee58cfc3066a5036886681de37957547b77
Luis García <garciafigueres@gmail.com>
2020-05-07 13:08:47 +0200

    65. URL for REST Request

---
+++ xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xamarin_udemy_travelrecordapp.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = @"WXEYTZWCBJKZCWVUQFAA2OFO2VPLKFCWKS2HNKND41JBRL5G";
+        public const string CLIENT_SECRET = @"ZG4RMINWSLOA0N425XB1H1IWFJWWEQXPYUQNRUPV0ZLDCKWO";
+    }
+}
---
+++ xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/Model/Venue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using xamarin_udemy_travelrecordapp.Helpers;
+
+namespace xamarin_udemy_travelrecordapp.Model
+{
+    public class Venue
+    {
+        public static string GenerateURL(double latitude, double longitude)
+        {            
+            return string.Format(Constants.VENUE_SEARCH, latitude, longitude, Constants.CLIENT_ID, Constants.CLIENT_SECRET, DateTime.Now.ToString("yyyyMMdd"));
+        }
+    }
+}
