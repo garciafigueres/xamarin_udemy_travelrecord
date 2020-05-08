01c40fab8d927b464effb24f1e2c9e716c2a1c30
Luis García <garciafigueres@gmail.com>
2020-05-08 16:59:44 +0200

    Linq Distinct

--- xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/ProfilePage.xaml.cs
+++ xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/ProfilePage.xaml.cs
@@ -2,6 +2,7 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using xamarin_udemy_travelrecordapp.Model;
+using System.Linq;
 
 namespace xamarin_udemy_travelrecordapp
 {
@@ -20,6 +21,11 @@ namespace xamarin_udemy_travelrecordapp
             using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
             {
                 var postTable = conn.Table<Post>().ToList();
+
+                var categories = (from p in postTable
+                                  orderby p.CategoryId
+                                  select p.CategoryName).Distinct().ToList();
+
                 postCountLabel.Text = postTable.Count.ToString();
             }
         }
