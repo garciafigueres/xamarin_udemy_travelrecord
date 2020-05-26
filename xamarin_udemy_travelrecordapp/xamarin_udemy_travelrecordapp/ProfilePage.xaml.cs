c9bbfaf7f082010916958973b76c13bd210fd475
Luis García <garciafigueres@gmail.com>
2020-05-26 09:21:25 +0200

    Linq with ListViews and Dictionaries

--- xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/ProfilePage.xaml.cs
+++ xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/ProfilePage.xaml.cs
@@ -3,6 +3,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using xamarin_udemy_travelrecordapp.Model;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace xamarin_udemy_travelrecordapp
 {
@@ -25,6 +26,21 @@ namespace xamarin_udemy_travelrecordapp
                 var categories = (from p in postTable
                                   orderby p.CategoryId
                                   select p.CategoryName).Distinct().ToList();
+
+                Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+                foreach (var category in categories)
+                {
+                    var count = (from post in postTable
+                                 where post.CategoryName == category
+                                 select post).ToList().Count();
+
+                    // Las expresiones de count y count2 son totalmente equivalentes
+                    //var count2 = postTable.Where(p => p.CategoryName == category).ToList().Count();
+
+                    categoriesCount.Add(category, count);
+                }
+
+                categoriesListView.ItemsSource = categoriesCount;
 
                 postCountLabel.Text = postTable.Count.ToString();
             }
