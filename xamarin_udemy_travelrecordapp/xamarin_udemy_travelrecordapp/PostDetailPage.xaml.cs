81f99eb7ead2c14e520ab94ea078fe788fee9b87
Luis García Figueres <garciafigueres@gmail.com>
2020-05-30 06:52:36 +0200

    Update and delete from Azure

--- xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/PostDetailPage.xaml.cs
+++ xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/PostDetailPage.xaml.cs
@@ -16,43 +16,62 @@ namespace xamarin_udemy_travelrecordapp
     {
         Post selectedPost;
 
+        public PostDetailPage()
+        {
+
+        }
+
         public PostDetailPage(Post selectedPost)
         {
             InitializeComponent();
             this.selectedPost = selectedPost;
+            
             experienceEntry.Text = selectedPost.Experience;
+            venueLabel.Text = selectedPost.VenueName;
+            categoryLabel.Text = selectedPost.CategoryName;
+            addressLabel.Text = selectedPost.Address;
+            coordinatesLabel.Text = $"{selectedPost.Latitude},{selectedPost.Longitude}";
+            distanceLabel.Text = $"{selectedPost.Distance} m";
         }
 
-        private void updateButton_Clicked(object sender, EventArgs e)
+        
+
+        private async void updateButton_Clicked(object sender, EventArgs e)
         {
             selectedPost.Experience = experienceEntry.Text;
 
-            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
-            {
-                conn.CreateTable<Post>();
-                int rows = conn.Update(selectedPost);
-                conn.Close();
+            //using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            //{
+            //    conn.CreateTable<Post>();
+            //    int rows = conn.Update(selectedPost);
+            //    conn.Close();
 
-                if (rows > 0)
-                    DisplayAlert("Success", "Experience succesfully updated", "Ok");
-                else
-                    DisplayAlert("Failure", "Experience failed to be updated", "Ok");
-            }
+            //    if (rows > 0)
+            //        DisplayAlert("Success", "Experience succesfully updated", "Ok");
+            //    else
+            //        DisplayAlert("Failure", "Experience failed to be updated", "Ok");
+            //}
+
+            await App.MobileService.GetTable<Post>().UpdateAsync(selectedPost);
+            await DisplayAlert("Success", "Experience succesfully updated", "Ok");
         }
 
-        private void DeleteButton_Clicked(object sender, EventArgs e)
+        private async void DeleteButton_Clicked(object sender, EventArgs e)
         {
-            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
-            {
-                conn.CreateTable<Post>();
-                int rows = conn.Delete(selectedPost);
-                conn.Close();
+            //using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            //{
+            //    conn.CreateTable<Post>();
+            //    int rows = conn.Delete(selectedPost);
+            //    conn.Close();
 
-                if (rows > 0)
-                    DisplayAlert("Success", "Experience succesfully deleted", "Ok");
-                else
-                    DisplayAlert("Failure", "Experience failed to be deleted", "Ok");
-            }
+            //    if (rows > 0)
+            //        DisplayAlert("Success", "Experience succesfully deleted", "Ok");
+            //    else
+            //        DisplayAlert("Failure", "Experience failed to be deleted", "Ok");
+            //}
+
+            await App.MobileService.GetTable<Post>().DeleteAsync(selectedPost);
+            await DisplayAlert("Success", "Experience succesfully deleted", "Ok");
         }
     }
 }
