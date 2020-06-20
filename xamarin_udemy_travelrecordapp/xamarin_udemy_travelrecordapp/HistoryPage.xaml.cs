172214137329a83244e4699b069f8bee6ea7f811
Luis García Figueres <garciafigueres@gmail.com>
2020-06-20 06:54:12 +0200

    ObservableCollection

--- xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/HistoryPage.xaml.cs
+++ xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/HistoryPage.xaml.cs
@@ -3,22 +3,27 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using xamarin_udemy_travelrecordapp.Model;
+using xamarin_udemy_travelrecordapp.ViewModel;
 
 namespace xamarin_udemy_travelrecordapp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HistoryPage : ContentPage
     {
+        HistoryVM viewModel;
         public HistoryPage()
         {
             InitializeComponent();
+
+            viewModel = new HistoryVM();
+            BindingContext = viewModel;
         }
 
-        protected override async void OnAppearing()
+        protected override void OnAppearing()
         {
             base.OnAppearing();
-            var posts = await Post.Read();
-            postListView.ItemsSource = posts;
+
+            viewModel.UpdatePosts();
         }
     }
 }
---
+++ xamarin_udemy_travelrecordapp/xamarin_udemy_travelrecordapp/ViewModel/HistoryVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using xamarin_udemy_travelrecordapp.Model;
+
+namespace xamarin_udemy_travelrecordapp.ViewModel
+{
+    public class HistoryVM
+    {
+        public ObservableCollection<Post> Posts { get; set; }
+
+        public HistoryVM()
+        {
+            Posts = new ObservableCollection<Post>();
+        }
+
+        public async void UpdatePosts()
+        {
+            var posts = await Post.Read();
+
+            if (posts != null)
+            {
+                Posts.Clear();
+                foreach (var post in posts)
+                    Posts.Add(post);
+            }
+        }
+    }
+}
